729b4e06287bb92cc05e2cc4775d56c3abb86797
Raghav sidhanti <sudohippie@users.noreply.github.com>
2015-08-11 23:15:50 -0700

    removing changes to sudoer, bad idea.

--- roles/deb-devel-tools/files/docker/update_sudoers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SUDOERS="/etc/sudoers"
-
-echo "\n" >> $SUDOERS
-echo "# enable running docker with out sudo" >> $SUDOERS
-echo "rsidhanti localhost = (root) NOPASSWD: /usr/bin/docker" >> $SUDOERS
