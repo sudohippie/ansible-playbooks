2250f23a0603d34e7655400620a0d2594a44f807
Raghav Sidhanti <sudohippie@users.noreply.github.com>
2016-08-17 13:38:16 -0700

    pandora id3 tag modifier

---
+++ ubuntu/roles/sudohippie-files/files/pandora_id3.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+TMP_FILE="$(pwd)/tmp_id3.sh"
+AUDIO_FORMAT=".mp4"
+
+# make sure the files are in the right format
+BAD_FILE_FORMATS=$(ls | grep ".* - .* - .* - ") 
+if [ -n "$BAD_FILE_FORMATS" ];
+then
+	echo -e "MANUAL ACTION REQUIRED: Fix the following file formats:\n"
+	echo "$BAD_FILE_FORMATS"
+	echo -e "\nNo changes were made to file ID3 tags."
+	exit 0
+fi
+
+# create executable
+ls -1 | grep $AUDIO_FORMAT | awk -F ' - ' '{print "id3 -a \""$1"\" -A \""$2"\" -t \""$3"\" `"$0"`"}' >> $TMP_FILE
+
+# modify data
+sed -i 's/.mp4"/"/g' $TMP_FILE
+sed -i 's/`/"/g' $TMP_FILE
+
+# execute
+chmod +x $TMP_FILE
+bash $TMP_FILE
+
+# cleanup
+rm $TMP_FILE
